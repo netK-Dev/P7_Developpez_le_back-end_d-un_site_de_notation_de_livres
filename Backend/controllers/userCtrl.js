77924e91b10cf564aadda70774b406df845236e7
netK-Dev <flyaxe709gmail.com>
2024-09-04 14:27:54 -1000

    création de la logique métier 'login'

--- Backend/controllers/userCtrl.js
+++ Backend/controllers/userCtrl.js
@@ -17,4 +17,29 @@ exports.signup = (req, res, next) => {
     .catch((error) => res.status(500).json({ error }));
 };
 
-exports.login = (req, res, next) => {};
+exports.login = (req, res, next) => {
+  userModel
+    .findOne({ email: req.body.email })
+    .then((user) => {
+      if (user === null) {
+        res.status(401).json({ message: 'Email ou mot de passe incorrect !' });
+      } else {
+        bcrypt
+          .compare(req.body.password, user.password)
+          .then((valid) => {
+            if (!valid) {
+              res
+                .status(401)
+                .json({ message: 'Email ou mot de passe incorrect !' });
+            } else {
+              res.status(200).json({
+                userId: user._id,
+                token: 'TOKEN', // Provisoir
+              });
+            }
+          })
+          .catch((error) => res.status(500).json({ error }));
+      }
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
