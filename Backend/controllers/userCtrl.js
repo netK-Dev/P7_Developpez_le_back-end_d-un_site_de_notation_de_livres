966bcb24992178930d86bf732aeb96b2ab8f8964
netK-Dev <flyaxe709gmail.com>
2024-09-04 14:47:39 -1000

    ajout de la logique de création du token dans notre fonction 'login'

--- Backend/controllers/userCtrl.js
+++ Backend/controllers/userCtrl.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const userModel = require('../models/userModel');
 
 exports.signup = (req, res, next) => {
@@ -34,7 +35,11 @@ exports.login = (req, res, next) => {
             } else {
               res.status(200).json({
                 userId: user._id,
-                token: 'TOKEN', // Provisoir
+                token: jwt.sign(
+                  { userId: user._id },
+                  'RANDOM_DEV_KEY', // Provisoire
+                  { expiresIn: '24h' }
+                ),
               });
             }
           })
