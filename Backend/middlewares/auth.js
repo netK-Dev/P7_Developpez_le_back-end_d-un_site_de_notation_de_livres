82b84abd3427706dbfabc7890c76b487b996eac6
netK-Dev <flyaxe709gmail.com>
2024-09-04 15:09:31 -1000

    création du middlewares d'authentification auth.js

---
+++ Backend/middlewares/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1]; // Récupérer le token après "Bearer"
+    const decodedToken = jwt.verify(token, 'RANDOM_DEV_KEY'); // Décrypter le token avec la clé
+    const userId = decodedToken.userId; // Extraire l'ID utilisateur du token
+    req.auth = {
+      userId: userId,
+    };
+    next();
+  } catch (error) {
+    res.status(401).json({ error: 'Requête non authentifiée !' });
+  }
+};
