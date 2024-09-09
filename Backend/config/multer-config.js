569f2dddf379b209338a9ac944c4e4ef7b9d9451
netK-Dev <flyaxe709gmail.com>
2024-09-09 11:15:30 -1000

    ajout de multer au projet + création du middleware de configuration multer

---
+++ Backend/config/multer-config.js
@@ -0,0 +1,24 @@
+const multer = require('multer');
+
+const MIME_TYPES = {
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpg',
+  'image/png': 'png',
+  'image/webp': 'webp',
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, 'images');
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(' ').join('_');
+    const extension = MIME_TYPES[file.mimetype];
+    if (!extension) {
+      return callback(new Error('Invalid file type'));
+    }
+    callback(null, name + Date.now() + '.' + extension);
+  },
+});
+
+module.exports = multer({ storage }).single('image');
